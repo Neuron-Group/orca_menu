9cc2dff08779bb71e4724aa709ed65fe6351eec8
Neuron-Group <2062512253@qq.com>
2026-05-05 11:28:47 +0800

    Added more integrate test.

---
+++ tests/integration/topbar_disabled_lualine_color.lua
@@ -0,0 +1,72 @@
+local H = dofile(vim.fn.getcwd() .. "/tests/helpers.lua")
+
+require("orca_menu").setup({
+  enable_mouse = true,
+  lualine = {
+    section = "y",
+    spacing = " ",
+  },
+  topbar = {
+    hint_format = "{hint}->{label}",
+  },
+  highlights = {
+    disabled = "Comment",
+    topbar_disabled = "Comment",
+  },
+  menus = {
+    {
+      label = "&Tools",
+      key = "t",
+      items = {
+        { label = "&Shell", key = "s", action = function() end },
+      },
+    },
+    {
+      label = "&LSP",
+      key = "p",
+      enabled = false,
+      items = {
+        { label = "&Rename", key = "r", action = function() end },
+      },
+    },
+    {
+      label = "&View",
+      key = "v",
+      items = {
+        { label = "&Tree", key = "t", action = function() end },
+      },
+    },
+  },
+})
+
+local lualine = require("lualine")
+local config = lualine.get_config()
+local section = config.sections.lualine_y
+local orca_components = {}
+
+H.truthy(section, "lualine_y section should be configured")
+for _, component in ipairs(section) do
+  if type(component) == "table" and component.orca_menu_component == true then
+    table.insert(orca_components, component)
+  end
+end
+H.eq(#orca_components, 4, "orca should register anchor plus one component per top menu")
+
+local disabled_component = orca_components[3]
+H.truthy(type(disabled_component) == "table", "disabled top menu component should be a lualine table component")
+H.truthy(type(disabled_component[1]) == "function", "disabled top menu component should expose a render callback")
+H.truthy(type(disabled_component.color) == "function", "disabled top menu component should expose a color callback")
+
+local disabled_text = disabled_component[1]()
+local disabled_color = disabled_component.color()
+local comment_hl = vim.api.nvim_get_hl(0, { name = "Comment", link = false })
+
+H.eq(disabled_text, " %@v:lua.orca_menu_click_menu_2@p->LSP%X", "disabled top menu should render without embedding a custom statusline highlight")
+H.eq(disabled_color.fg, string.format("#%06x", comment_hl.fg), "disabled top menu should hand lualine the configured foreground color")
+H.eq(disabled_color.bg, nil, "disabled top menu color should leave background unset so lualine keeps its own section background")
+
+local enabled_component = orca_components[2]
+H.eq(enabled_component.color(), nil, "enabled top menu should not override lualine section colors")
+
+H.finish()
+print("ok - tests/integration/topbar_disabled_lualine_color.lua")
