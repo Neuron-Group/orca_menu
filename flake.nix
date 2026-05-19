4cc526e3d3d016792e9d52862fb20e92f363e1d1
Neuron-Group <2062512253@qq.com>
2026-05-19 15:56:13 +0800

    Fix some problems which already exposed.

--- flake.nix
+++ flake.nix
@@ -11,6 +11,16 @@
       src = builtins.path {
         path = ./.;
         name = "orca-menu-src";
+        filter = path: type:
+          let
+            baseName = baseNameOf path;
+          in
+            baseName != "result"
+            && baseName != ".git"
+            && baseName != ".nvimlog"
+            && baseName != ".tmp-orca-bootstrap-trace.log"
+            && baseName != ".tmp-notify-exact-trace.log"
+            && baseName != ".tmp-data";
       };
       overlay = final: prev: {
         vimPlugins = prev.vimPlugins // {
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,9 +1,25 @@
+let
+  src = builtins.path {
+    path = ../.;
+    name = "orca-menu-src";
+    filter = path: type:
+      let
+        baseName = baseNameOf path;
+      in
+        baseName != "result"
+        && baseName != ".git"
+        && baseName != ".nvimlog"
+        && baseName != ".tmp-orca-bootstrap-trace.log"
+        && baseName != ".tmp-notify-exact-trace.log"
+        && baseName != ".tmp-data";
+  };
+in
 final: prev: {
   vimPlugins = prev.vimPlugins // {
     orca-menu = final.vimUtils.buildVimPlugin {
       pname = "orca-menu";
       version = "dev";
-      src = ../.;
+      src = src;
     };
   };
 }
